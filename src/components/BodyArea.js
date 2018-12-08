952b5648fd0e26abf8a2869f19712959ea8d14a1
Supreeth D <supreeth.d@gieom.com>
2018-12-08 19:52:37 +0530

    Adding Like and delete  methods and other minor change

--- src/components/BodyArea.js
+++ src/components/BodyArea.js
@@ -3,19 +3,17 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
-
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
-import red from '@material-ui/core/colors/red';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 import ShareIcon from '@material-ui/icons/Share';
+import CardContent from '@material-ui/core/CardContent';
 
-
-
+import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
   root: {
@@ -33,13 +31,13 @@ const styles = theme => ({
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '100%', // 1:1
   },
   actions: {
     display: 'flex',
   },
-  avatar: {
-    backgroundColor: red[500],
+  fontStyle: {
+    fontStyle: 'italic',
   }
 });
 
@@ -52,7 +50,22 @@ class CardsArea extends React.Component {
     };
   }
 
- 
+updateLikeCount = (indx) => {
+let updatedData=this.state.imageData;
+updatedData[indx].likes=updatedData[indx].likes + 1;
+  this.setState({
+  imageData:updatedData
+})
+
+}
+removeCard = (indx) => {
+  let updatedData=this.state.imageData;
+  updatedData.splice(indx, 1);
+    this.setState({
+    imageData:updatedData
+  })
+  
+  }
 
   render() {
     const { classes } = this.props;
@@ -66,22 +79,25 @@ class CardsArea extends React.Component {
       <div className={classes.root} >
       { imageData.map((n,indx) => {
                 return (
-                  <Card className={classes.card}>
+                  <Card className={classes.card} key={indx}>
                 
                   <CardMedia
                     className={classes.media}
                     image={n.Image}
                   />
-              
+                    <CardContent>
+          <Typography className={classes.fontStyle} component="p">{ "Timestamp - " + n.timestamp}
+          </Typography>
+        </CardContent>
                   <CardActions className={classes.actions} disableActionSpacing>
                     {n.likes}
-                    <IconButton aria-label="Like">
+                    <IconButton aria-label="Like" onClick={()=>this.updateLikeCount(indx)}>
                       <FavoriteIcon />
                     </IconButton>
                     <IconButton aria-label="Share">
                       <ShareIcon />
                     </IconButton>
-                    <IconButton aria-label="Delete">
+                    <IconButton aria-label="Delete"   onClick={()=>this.removeCard(indx)}>
                       <DeleteIcon />
                     </IconButton>
                   </CardActions>
